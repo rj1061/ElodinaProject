5a5b8743f1317817c430a94825acc80722dbf4b0
Rishabh Jain <rj1061@nyu.edu>
2016-03-15 15:48:57 -0400

    Modular untested code

--- ebay_bidding.py
+++ ebay_bidding.py
@@ -3,22 +3,35 @@ import sys
 from ebaysdk.exception import ConnectionError
 from ebaysdk.finding import Connection
 
-try:
-    api = Connection(appid='Elodina-Elodina-PRD-dd2d4c776-3a2daa78', config_file=None)
-    response = api.execute('findItemsAdvanced', {'keywords': sys.argv[1]})
+class ebayAPICall:
 
-    print type(response)
-    f = open('response', 'w')
-    f.write(response.json())
-    assert(response.reply.ack == 'Success')
-    assert(type(response.reply.timestamp) == datetime.datetime)
-    assert(type(response.reply.searchResult.item) == list)
+    def findItemsAdvanced(api, keywords):
+        try:
+            response = api.execute('findItemsAdvanced', {'keywords': sys.argv[1]})
 
-    item = response.reply.searchResult.item[0]
-    assert(type(item.listingInfo.endTime) == datetime.datetime)
-    assert(type(response.dict()) == dict)
-    print("Success\n")
+            print type(response)
+            f = open('response', 'w')
+            f.write(response.json())
+            assert(response.reply.ack == 'Success')
+            assert(type(response.reply.timestamp) == datetime.datetime)
+            assert(type(response.reply.searchResult.item) == list)
 
-except ConnectionError as e:
-    print(e)
-    print(e.response.dict())
+            item = response.reply.searchResult.item[0]
+            assert(type(item.listingInfo.endTime) == datetime.datetime)
+            assert(type(response.dict()) == dict)
+            print("Success\n")
+
+        except ConnectionError as e:
+            print(e)
+            print(e.response.dict())
+
+    def getCompletedItemsByKeyword(api, keywords):
+        try:
+            response = api.execute('findCompletedItems', {'keywords': keywords})
+            print type(response)
+        except ConnectionError as e:
+            print(e)
+            print(e.response.dict())
+
+    def __init__(self):
+        self.api = Connection(appid='Elodina-Elodina-PRD-dd2d4c776-3a2daa78', config_file=None)
